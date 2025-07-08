3d7ee0fc73c30d52a321d38f3e334318c67c64ef
Vishalkumar0023 <vishal.kumar.connect0@gmail.com>
2025-07-08 14:39:45 +0530

    Add initial file

---
+++ python.py
@@ -0,0 +1,10 @@
+def find_max(nums):
+    max_num=nums[0]
+    for num in nums:
+        if num > max_num:
+            max_num=num
+    return max_num
+nums = [10, 5, 30, 8, 20]
+print("Maximum number:", find_max(nums))        
+
+
